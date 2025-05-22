8dc45164672e689ce78b990fdbabd5c378c29a78
ardhikaxx <ardhikayanuar58@gmail.com>
2025-05-23 05:19:37 +0700

    update- fix nama

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Diabetic Management',
+      title: 'GlucoWise',
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
